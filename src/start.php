e0c3edc998b71334e4574fc597d8dd8b177057b2
Alex Comin <alexandr.komin91@gmail.com>
2016-09-05 17:44:41 +0300

    Update start.php

--- src/start.php
+++ src/start.php
@@ -2,7 +2,7 @@
 
 include('classSync.php');
 
-$sync = new Sync('localhost', 'alex', 'ooZ6re', 'alex', 'table_full.sql');
+$sync = new Sync('localhost', 'alex', '123456', 'alex', 'table_full.sql');
 $sync->start_sync();
 
 ?>
